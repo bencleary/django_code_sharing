5024663d13f2c5086673b02342acb9e689dc7e48
Ben Cleary <me@bencleary.dev>
2020-04-09 10:52:35 +0100

    Added extra fields to snippet

--- src/snippet/models.py
+++ src/snippet/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from uuid import uuid4
+from django.utils.translation import ugettext_lazy as _
 
 
 class Snippet(models.Model):
-    urn = models.UUIDField(default=uuid4)
-    content = models.TextField()
+    urn = models.UUIDField(_("URN"), default=uuid4)
+    syntax = models.CharField(_("Syntax"), max_length=75, blank=True)
+    description = models.TextField(_("Description"), blank=True)
+    content = models.TextField(_("Content"))
     created_at = models.DateTimeField(auto_now_add=True)
     expiry = models.DateTimeField(blank=True, null=True)
