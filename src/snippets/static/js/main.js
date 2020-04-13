282c88110218a75c2ce6d90cd3a6f0767c84298e
Ben Cleary <me@bencleary.dev>
2020-04-13 13:59:49 +0100

    light and dark mode toggle

--- src/snippets/static/js/main.js
+++ src/snippets/static/js/main.js
@@ -65,3 +65,77 @@ function updateTextArea() {
 }
 
 updateTextArea();
+
+
+function toggleDarkMode() {
+
+    if (localStorage.getItem('mode') == 'LIGHT') {
+        localStorage.setItem('mode', 'DARK');
+    } else {
+        localStorage.setItem('mode', 'LIGHT');
+    }
+
+    switch (localStorage.getItem('mode')) {
+        case 'LIGHT':
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('mode', 'LIGHT');
+            break;
+        case 'DARK':
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('mode', 'DARK');
+            break;
+    }
+    toggleButtonIcon();
+    updateButtonTitle();
+    updateLogo();
+}
+
+function toggleButtonIcon() {
+    switch (localStorage.getItem('mode')) {
+        case 'DARK':
+            document.getElementById('light-dark-toggle').innerHTML = '<i class="fas fa-sun"></i>';
+            break;
+        case 'LIGHT':
+            document.getElementById('light-dark-toggle').innerHTML = '<i class="fas fa-moon"></i>';
+            break;
+    }
+
+}
+
+function updateButtonTitle() {
+    switch (localStorage.getItem('mode')) {
+        case 'DARK':
+            document.getElementById('light-dark-toggle').title = 'Enable Light Mode';
+            break;
+        case 'LIGHT':
+            document.getElementById('light-dark-toggle').title = 'Enable Dark Mode';
+            break;
+    }
+}
+
+function updateLogo() {
+    switch (localStorage.getItem('mode')) {
+        case 'DARK':
+            document.getElementById('snip-logo').src = "/static/imgs/logo-light.png";
+            break;
+        case 'LIGHT':
+            document.getElementById('snip-logo').src = "/static/imgs/logo.png";
+            break;
+    }
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    switch (localStorage.getItem('mode')) {
+        case 'LIGHT':
+            document.documentElement.setAttribute('data-theme', 'light');
+            localStorage.setItem('mode', 'LIGHT');
+            break;
+        case 'DARK':
+            document.documentElement.setAttribute('data-theme', 'dark');
+            localStorage.setItem('mode', 'DARK');
+            break;
+    }
+    toggleButtonIcon();
+    updateButtonTitle();
+    updateLogo();
+});
